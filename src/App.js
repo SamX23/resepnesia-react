8b613a9f55fea82b3a7137cf63b22712d5799dd3
SamX23 <skalexsong@gmail.com>
2021-08-31 22:54:29 +0700

    Update : create basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+const header_grid = {
+  display: "flex",
+  justifyContent: "space-between",
+  margin: "0 2rem",
+};
+
+const nav_left = {};
+const nav_right = {};
+
+const footer_container = {
+  margin: "0 2rem",
+};
+
+const footer_grid = {
+  display: "grid",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header style={header_grid}>
+        <nav style={nav_left}>
+          <img />
+          <span>Resep</span>
+          <span>Daerah</span>
+          <span>Blog</span>
+          <span>Tentang</span>
+        </nav>
+        <nav style={nav_right}>
+          <span>Daftar</span>
+          <span>Masuk</span>
+          <i className="icon-search"></i>
+          <i className="icon-bookmark"></i>
+        </nav>
       </header>
-    </div>
+      <main className="main_grid">
+        <div>Hero section</div>
+        <div>Populer section</div>
+        <div>Video highlight section</div>
+        <div>Memasak item section</div>
+        <div>Daerah section</div>
+        <div>Blog tips section</div>
+        <div>Quote section</div>
+        <div>Blog section</div>
+        <div>Review section</div>
+      </main>
+      <footer style={footer_container}>
+        <div style={footer_grid}>
+          <div>Footer desc</div>
+          <div>Footer nav</div>
+          <div>Footer logo</div>
+          <div>Footer utils</div>
+        </div>
+        <div>Footer copyright</div>
+      </footer>
+    </>
   );
 }
 
